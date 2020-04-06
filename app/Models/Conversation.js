7e4eba20282a50819368063c01d9113cbfda3657
Caleb Mathew <creatrixity@gmail.com>
2020-04-06 22:07:58 +0100

    feat: add role_label to messages

--- app/Models/Conversation.js
+++ app/Models/Conversation.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Conversation extends Model {
-    conversationMessage() {
-        return this.hasMany("App/Models/ConversationMessage", "id", "conversation_id")
-    }
-
-    user() {
-        return this.hasOne('App/Models/User', 'user_id', 'id')
-    }
+  conversationMessage() {
+    return this.hasMany(
+      "App/Models/ConversationMessage",
+      "id",
+      "conversation_id"
+    );
+  }
 }
 
-module.exports = Conversation
+module.exports = Conversation;
--- app/Models/ConversationMessage.js
+++ app/Models/ConversationMessage.js
@@ -1,9 +1,12 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class ConversationMessage extends Model {
+  user() {
+    return this.hasOne("App/Models/User", "user_id", "id");
+  }
 }
 
-module.exports = ConversationMessage
+module.exports = ConversationMessage;
