3cc229358a8caf298d3b378832a12c652bc48440
Caleb Mathew <creatrixity@gmail.com>
2020-04-09 15:08:05 +0100

    update edit product feature

--- app/Controllers/Features/EditProductFeature.js
+++ app/Controllers/Features/EditProductFeature.js
@@ -1,126 +1,126 @@
 "use strict";
 const StoreProduct = use("App/Models/StoreProduct");
 const Store = use("App/Models/Store");
-const {
-	uploadImage
-} = use("App/HelperFunctions/UploadImage");
+const { uploadImage } = use("App/HelperFunctions/UploadImage");
 const Image = use("App/Models/Image");
 const ProductTag = use("App/Models/ProductTag");
 
 class EditProductFeature {
-	constructor(request, response, auth) {
-		this.request = request;
-		this.response = response;
-		this.auth = auth;
-	}
+  constructor(request, response, auth) {
+    this.request = request;
+    this.response = response;
+    this.auth = auth;
+  }
 
-	async processTags({
-		tags,
-		productId
-	}) {
-		if (tags) {
-			const existingTags = await ProductTag.findBy("product_id", productId);
-			await existingTags.delete();
-			for (var tag in tags) {
-				const productTag = new ProductTag()
+  async processTags({ tags, productId }) {
+    if (tags) {
+      const existingTags = await ProductTag.findBy("product_id", productId);
+      await existingTags.delete();
+      for (var tag in tags) {
+        const productTag = new ProductTag();
 
-				productTag.product_id = productId
-				productTag.tag = tags[tag]
+        productTag.product_id = productId;
+        productTag.tag = tags[tag];
 
-				await productTag.save()
-			}
-		}
-	}
+        await productTag.save();
+      }
+    }
+  }
 
   /**
    *
    * @param {Integer} product_id - id of the product
    * @return {Object} -response -ctx
    */
-	async editProduct(productId) {
-		try {
-			const user = this.auth.current.user;
-			const user_id = user.id;
-			const user_store = await Store.findBy("user_id", user_id);
-			const {
-				product_name,
-				description,
-				stock,
-				discount,
-				category_id,
-				subcategory_id,
-				is_published,
-				tag,
-				price
-			} = this.request.all();
+  async editProduct(productId) {
+    try {
+      let tags;
+      const user = this.auth.current.user;
+      const user_id = user.id;
+      const user_store = await Store.findBy("user_id", user_id);
+      const {
+        product_name,
+        description,
+        stock,
+        discount,
+        category_id,
+        subcategory_id,
+        is_published,
+        tag,
+        price
+      } = this.request.all();
 
-			if (user_store.is_activated_at == null) {
-				return this.response.status(400).send({
-					status: "Fail",
-					message: "Store is inactive please contact the admin",
-					status_code: 400
-				});
-			}
-			const tags = JSON.parse(tag);
-			const productImage = this.request.file("product_image", {
-				types: ["image"]
-			});
+      if (user_store.is_activated_at == null) {
+        return this.response.status(400).send({
+          status: "Fail",
+          message: "Store is inactive please contact the admin",
+          status_code: 400
+        });
+      }
+      if (tag) {
+        tags = JSON.parse(tag);
+      }
+      const productImage = this.request.file("product_image", {
+        types: ["image"]
+      });
 
-			const product = await StoreProduct.findBy("id", productId);
-			product.merge({
-				product_name,
-				description,
-				price,
-				stock,
-				discount,
-				category_id,
-				subcategory_id,
-				is_enabled: is_published
-			});
-			await product.save();
+      const product = await StoreProduct.findBy("id", productId);
+      product.merge({
+        product_name,
+        description,
+        price,
+        stock,
+        discount,
+        category_id,
+        subcategory_id,
+        is_enabled: is_published
+      });
+      await product.save();
 
-			const imagesIds = [];
+      const imagesIds = [];
 
-			if (productImage) {
-				const mutipleImage = productImage._files;
-				if (!mutipleImage) {
-					const uploadedImage = await uploadImage(productImage);
-					const newImage = new Image();
-					newImage.image_url = uploadedImage.url;
-					await newImage.save();
-					imagesIds.push(newImage.id);
-				} else {
-					for (var image in mutipleImage) {
-						const uploaded_image = await uploadImage(mutiple_image[image]);
-						const newImage = new Image();
-						newImage.image_url = uploaded_image.url;
-						await newImage.save();
-						imagesIds.push(newImage.id);
-					}
-				}
-			}
+      if (productImage) {
+        const mutipleImage = productImage._files;
+        if (!mutipleImage) {
+          const uploadedImage = await uploadImage(productImage);
+          const newImage = new Image();
+          newImage.image_url = uploadedImage.url;
+          await newImage.save();
+          imagesIds.push(newImage.id);
+        } else {
+          for (var image in mutipleImage) {
+            const uploaded_image = await uploadImage(mutiple_image[image]);
+            const newImage = new Image();
+            newImage.image_url = uploaded_image.url;
+            await newImage.save();
+            imagesIds.push(newImage.id);
+          }
+        }
+      }
 
-			await this.processTags({
-				tags,
-				productId
-			})
+      if (tag) {
+        await this.processTags({
+          tags,
+          productId
+        });
+      }
 
-			//add to pivot table
-			await product.main_product_images().sync(imagesIds);
-			return this.response.status(200).send({
-				message: "Product successfully updated",
-				status_code: 200,
-				status: "Success",
-				product
-			});
-		} catch (editProductError) {
-			console.log("editProductError", editProductError);
-			return this.response.status(500).send({
-				status: "Fail",
-				message: "Internal Server Error",
-				status_code: 500
-			});
-		}
-	}
+      //add to pivot table
+      await product.main_product_images().sync(imagesIds);
+      return this.response.status(200).send({
+        message: "Product successfully updated",
+        status_code: 200,
+        status: "Success",
+        product
+      });
+    } catch (editProductError) {
+      console.log("editProductError", editProductError);
+      return this.response.status(500).send({
+        status: "Fail",
+        message: "Internal Server Error",
+        status_code: 500
+      });
+    }
+  }
 }
 module.exports = EditProductFeature;
--- app/Controllers/Features/FetchProductInStoreFeature.js
+++ app/Controllers/Features/FetchProductInStoreFeature.js
@@ -3,52 +3,50 @@ const StoreProduct = use("App/Models/StoreProduct");
 const Store = use("App/Models/Store");
 
 class FetchProductInStoreFeature {
-	constructor(request, response) {
-		this.request = request;
-		this.response = response;
-	}
+  constructor(request, response, auth) {
+    this.request = request;
+    this.response = response;
+    this.auth = auth;
+  }
 
-	async fetchProduct() {
-		try {
-			const {
-				page,
-				limit
-			} = this.request.get();
+  async fetchProduct() {
+    try {
+      const { page, limit } = this.request.get();
 
-			const { id } = await this.auth.current.user
+      const { id } = await this.auth.current.user;
 
-			const store = await Store.query()
-				.where("user_id", id)
-				.first()
+      const store = await Store.query()
+        .where("user_id", id)
+        .first();
 
-			const storeId = store.id
+      const storeId = store.id;
 
-			const produceInStore = await StoreProduct.query()
-				.whereNull("deleted_at")
-				.andWhere("store_id", storeId)
-				.with("main_product_images")
-				.with("category")
-				.with("sub_category")
-				.with("store")
-				.with("tags")
-				.paginate(page, limit);
+      const produceInStore = await StoreProduct.query()
+        .whereNull("deleted_at")
+        .andWhere("store_id", storeId)
+        .with("main_product_images")
+        .with("category")
+        .with("sub_category")
+        .with("store")
+        .with("tags")
+        .paginate(page, limit);
 
-			const serializedProduct = produceInStore.toJSON()
+      const serializedProduct = produceInStore.toJSON();
 
-			this.response.status(200).send({
-				message: "Successfully fetch all products",
-				status: "success",
-				status_code: 200,
-				results: serializedProduct
-			});
-		} catch (fetchProduceError) {
-			console.log("fetchProduceError", fetchProduceError);
-			return this.response.status(500).send({
-				status: "fail",
-				status_code: 500,
-				message: "Internal Server Error"
-			});
-		}
-	}
+      this.response.status(200).send({
+        message: "Successfully fetch all products",
+        status: "success",
+        status_code: 200,
+        results: serializedProduct
+      });
+    } catch (fetchProduceError) {
+      console.log("fetchProduceError", fetchProduceError);
+      return this.response.status(500).send({
+        status: "fail",
+        status_code: 500,
+        message: "Internal Server Error"
+      });
+    }
+  }
 }
 module.exports = FetchProductInStoreFeature;
