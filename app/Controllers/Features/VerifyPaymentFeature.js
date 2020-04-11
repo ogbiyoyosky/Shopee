d487f741911086c7305e139de29aec287ddcabf5
Emmanuel Ogbiyoyo <emmanuel.o@crennettechlabs.com>
2020-04-11 16:34:28 +0100

    fix redirect

--- app/Controllers/Features/VerifyPaymentFeature.js
+++ app/Controllers/Features/VerifyPaymentFeature.js
@@ -1,43 +1,39 @@
-'use strict'
-const Config = use('Config')
-const Env = use('Env')
+"use strict";
+const Config = use("Config");
+const Env = use("Env");
 const requestPromise = require("request-promise");
-const TransactionToken = use('App/Models/TransactionToken')
+const TransactionToken = use("App/Models/TransactionToken");
 
 class VerifyPaymentFeature {
   constructor(request, response) {
-    this.request = request
-    this.response = response
-
+    this.request = request;
+    this.response = response;
   }
 
   async verify() {
-    const {
-      reference
-    } = this.request.all()
-
+    const { reference } = this.request.all();
 
     const requestConfig = {
-      uri: Config.get(
-        "endpoints.paystack.verifyTransactionEndpoint"
-      ) + "/" + reference,
+      uri:
+        Config.get("endpoints.paystack.verifyTransactionEndpoint") +
+        "/" +
+        reference,
       headers: {
-        authorization: `Bearer ${Env.get('PAYSTACK_SECRET')}`,
+        authorization: `Bearer ${Env.get("PAYSTACK_SECRET")}`,
         "content-type": "application/json",
-        "cache-control": "no-cache"
+        "cache-control": "no-cache",
       },
       json: true,
     };
 
-
     return requestPromise(requestConfig)
-      .then(async apiResponse => {
+      .then(async (apiResponse) => {
         if (!apiResponse.status) {
           return this.response.status(400).send({
             status: "Fail",
             message: "Error contacting paystack",
-            status_code: 400
-          })
+            status_code: 400,
+          });
         }
 
         const fields = apiResponse.data.metadata.custom_fields;
@@ -51,26 +47,22 @@ class VerifyPaymentFeature {
 
         redirectQueryString = encodeURI(redirectQueryString.join("&"));
 
-        let user_id
-        let token
+        let user_id;
+        let token;
 
         let mapData = fields.map((item) => {
-          if (item.variable_name == 'tkn') {
-            token = item.value
+          if (item.variable_name == "tkn") {
+            token = item.value;
           }
-          if (item.variable_name == 'uid') {
-            user_id = item.value
+          if (item.variable_name == "uid") {
+            user_id = item.value;
           }
+        });
 
-        })
-
-
-
-        const transaction = new TransactionToken()
-        transaction.token = token
-        transaction.user_id = user_id
-        await transaction.save()
-
+        const transaction = new TransactionToken();
+        transaction.token = token;
+        transaction.user_id = user_id;
+        await transaction.save();
 
         const redirectURL = `${Config.get(
           "endpoints.transactions.processTransactionEndpoint"
@@ -79,15 +71,13 @@ class VerifyPaymentFeature {
         return this.response.redirect(redirectURL);
       })
       .catch((e) => {
-        console.log("Verify Payment Error", e)
-        return this.response.status(500).send({
+        console.log("Verify Payment Error", e);
+        return this.response.status(400).send({
           status: "Fail",
-          message: "Internal Server Error",
-          status_code: 500
-        })
+          message: "Unable to process transaction.",
+          status_code: 400,
+        });
       });
   }
-
-
 }
-module.exports = VerifyPaymentFeature
+module.exports = VerifyPaymentFeature;
