c307b4a8a32f0aa3d935c74f7ec9a6b74f0d02a5
Emmanuel Ogbiyoyo <emmanuel.o@crennettechlabs.com>
2020-04-12 00:28:24 +0100

    updated seller info

--- app/Controllers/Features/Notification/FetchSellerOrderNotificationFeature.js
+++ app/Controllers/Features/Notification/FetchSellerOrderNotificationFeature.js
@@ -1,5 +1,6 @@
 "use strict";
 const Database = use("Database");
+const OrderProduct = use("App/Models/OrderProduct");
 
 class NotificationFetchSellerOrderNotificationFeature {
   constructor(request, response, auth) {
@@ -11,7 +12,7 @@ class NotificationFetchSellerOrderNotificationFeature {
   async fetchSellerOrderNotifications() {
     try {
       const userId = this.auth.current.user.id;
-      const orderNotification = await Database.from("order_notifications")
+      let orderNotification = await Database.from("order_notifications")
         .select(
           "orders.id as order_id",
           "buyer_id",
@@ -24,30 +25,34 @@ class NotificationFetchSellerOrderNotificationFeature {
           "orders.buyer_confirms_delivery_at",
           "orders.shipping_cost",
           "orders.vat",
-          "orders.service_charge",
-          "store_products.product_name",
-          "order_products.product_id"
+          "orders.service_charge"
         )
         .where("seller_id", userId)
         .innerJoin("users", "order_notifications.buyer_id", "users.id")
-        .innerJoin(
-          "order_products",
-          "order_notifications.order_id",
-          "order_products.id"
-        )
+
         .innerJoin("orders", "order_notifications.order_id", "orders.id")
-        .innerJoin(
-          "store_products",
-          "order_products.product_id",
-          "store_products.id"
-        )
         .orderBy("orders.created_at", "desc");
+
+      for (var order in orderNotification) {
+        const orderDetails = await OrderProduct.query()
+          .where("order_id", orderNotification[order].order_id)
+          .with("products")
+          .fetch();
+        const serializedOrderDetails = orderDetails.toJSON();
+
+        orderNotification[order].products = serializedOrderDetails;
+
+        const query = await Database.from("order_products")
+          .where("order_id", orderNotification[order].order_id)
+          .count();
+        orderNotification[order].product_count = query[0]["count(*)"];
+      }
 
       return this.response.status(200).send({
         message: "Successfully returned all order notifications",
         status_code: 200,
         status: "success",
-        results: orderNotification
+        results: orderNotification,
       });
     } catch (fetchSellerOrderNotificationsError) {
       console.log(
@@ -57,7 +62,7 @@ class NotificationFetchSellerOrderNotificationFeature {
       return this.response.status(500).send({
         status: "Fail",
         message: "Internal Server Error",
-        status_code: 500
+        status_code: 500,
       });
     }
   }
