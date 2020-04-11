55451d90bd1caf8aad5863df02dbf0786a3bb4a3
Emmanuel Ogbiyoyo <emmanuel.o@crennettechlabs.com>
2020-04-11 19:57:32 +0100

    fix migration.

--- database/migrations/1578755819453_store_product_schema.js
+++ database/migrations/1578755819453_store_product_schema.js
@@ -1,29 +1,29 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class StoreProductSchema extends Schema {
-	up() {
-		this.create('store_products', (table) => {
-			table.increments()
-			table.integer('store_id')
-			table.string('product_name')
-			table.string('description')
-			table.integer('stock')
-			table.float('price')
-			table.integer("discount").defaultTo(0)
-			table.integer('category_id')
-			table.integer('subcategory_id')
-			table.boolean('is_enabled')
-			table.dateTime('deleted_at').defaultTo(null)
-			table.timestamps()
-		})
-	}
+  up() {
+    this.create("store_products", (table) => {
+      table.increments();
+      table.integer("store_id");
+      table.string("product_name");
+      table.text("description", "longtext");
+      table.integer("stock");
+      table.float("price");
+      table.integer("discount").defaultTo(0);
+      table.integer("category_id");
+      table.integer("subcategory_id");
+      table.boolean("is_enabled");
+      table.dateTime("deleted_at").defaultTo(null);
+      table.timestamps();
+    });
+  }
 
-	down() {
-		this.drop('store_products')
-	}
+  down() {
+    this.drop("store_products");
+  }
 }
 
-module.exports = StoreProductSchema
+module.exports = StoreProductSchema;
