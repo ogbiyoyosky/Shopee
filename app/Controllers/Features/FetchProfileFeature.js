cfc9d17e686e5270acef7212b8a42783c6f0f72a
Caleb Mathew <creatrixity@gmail.com>
2020-04-11 10:13:53 +0100

    fix: profile data returns seller details

--- app/Controllers/Features/FetchProfileFeature.js
+++ app/Controllers/Features/FetchProfileFeature.js
@@ -1,6 +1,6 @@
 "use strict";
 const User = use("App/Models/User");
-const moment = require("moment")
+const moment = require("moment");
 
 class FetchProfileFeature {
   constructor(request, response, auth) {
@@ -21,6 +21,8 @@ class FetchProfileFeature {
           "users.is_activated_at",
           // "country_codes.dial_code",
           "users.phone_number ",
+          "users.last_login_time ",
+          "users.last_updated_item",
           "role_label",
           "first_name",
           "last_name",
@@ -46,9 +48,9 @@ class FetchProfileFeature {
 
       const serializedResult = profile.toJSON();
 
-      const user = await User.findBy("id", user_id)
-      user.last_login_time = moment().format("YYYY-MM-DD  HH:mm:ss")
-      await user.save()
+      const user = await User.findBy("id", user_id);
+      user.last_login_time = moment().format("YYYY-MM-DD  HH:mm:ss");
+      await user.save();
 
       return this.response.status(200).send({
         message: "Successfully fetched the users profile",
