436d8a8b36e13f36714dc1e8f815f02f189872f2
Caleb Mathew <creatrixity@gmail.com>
2020-07-14 09:27:30 +0100

    feat: mock analytical data

--- app/Models/OrderNotification.js
+++ app/Models/OrderNotification.js
@@ -1,32 +1,38 @@
-<<<<<<< HEAD
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class OrderNotification extends Model {
-    buyer_details() {
-        return this.hasOne("App/Models/User", 'buyer_id', "id")
-    }
+  buyer_details() {
+    return this.hasOne("App/Models/User", "buyer_id", "id");
+  }
 
-    seller_details() {
-        return this.hasOne("App/Models/User", 'seller_id', "id")
-    }
+  seller_details() {
+    return this.hasOne("App/Models/User", "seller_id", "id");
+  }
 
-    order_items() {
-        return this.belongsToMany("App/Models/StoreProduct", "order_id", "product_id")
-            .pivotTable('order_products')
-            .withPivot(['qty'])
-    }
+  order_items() {
+    return this.belongsToMany(
+      "App/Models/StoreProduct",
+      "order_id",
+      "product_id"
+    )
+      .pivotTable("order_products")
+      .withPivot(["qty"]);
+  }
 
-    order_address() {
-        return this.belongsToMany("App/Models/Address", "address_id", "order_id").pivotTable('order_addresses')
-    }
+  order_address() {
+    return this.belongsToMany(
+      "App/Models/Address",
+      "address_id",
+      "order_id"
+    ).pivotTable("order_addresses");
+  }
 }
 
-module.exports = OrderNotification
-=======
-"use strict";
+module.exports = OrderNotification;
+("use strict");
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
@@ -62,4 +68,3 @@ class OrderNotification extends Model {
 }
 
 module.exports = OrderNotification;
->>>>>>> fe1e8007f0a27a66da49447ea8ae260a3872da09
