3c992c5fded2a25b55eafafa30ac7beca4b73c39
Sir Freeman <emmanuel.o@crenettechlabs.com>
2020-07-25 15:40:41 +0100

    Add email on adding shipping fee

--- app/Controllers/Features/Order/AddShippingCostOnOrderFeature.js
+++ app/Controllers/Features/Order/AddShippingCostOnOrderFeature.js
@@ -1,6 +1,9 @@
 "use strict";
 const Order = use("App/Models/Order");
+const Profile = use("App/Models/Profile");
+const User = use("App/Models/User");
 const moment = require("moment");
+const Event = use("Event");
 
 class OrderAddShippingCostOnOrderFeature {
   constructor(request, response, auth) {
@@ -17,17 +20,32 @@ class OrderAddShippingCostOnOrderFeature {
       orderDetail.seller_accepted_at = moment().format("YYYY-MM-DD HH:mm:ss");
       await orderDetail.save();
 
+      const userDetails = await Profile.findBy("id", orderDetail.user_id);
+      const user = await User.findBy("id", orderDetail.user_id);
+
+      const mailDetails = {
+        first_name: userDetails.first_name,
+        last_name: userDetails.last_name,
+        email: user.email,
+        shipping: shipping_cost,
+        vat: orderDetail.vat,
+        amount: orderDetail.amount,
+        total: shipping_cost + orderDetail.vat + orderDetail.amount,
+      };
+
+      Event.fire("new:total_invoice", mailDetails);
+
       return this.response.status(200).send({
         message: "Successfully added a shipping fee.",
         status: "success",
-        status_code: 200
+        status_code: 200,
       });
     } catch (addShippigCostError) {
       console.log("addShippigCostError", addShippigCostError);
       return this.response.status(500).send({
         status: "Fail",
         message: "Internal Server Error",
-        status_code: 500
+        status_code: 500,
       });
     }
   }
