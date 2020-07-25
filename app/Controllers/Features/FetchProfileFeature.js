4628af8a62ab2f359491ea8e059975eb3c1679a5
Caleb Mathew <caleb@crenettechlabs.com>
2020-07-25 11:37:07 +0100

    feat: fix profile feature

--- app/Controllers/Features/FetchProfileFeature.js
+++ app/Controllers/Features/FetchProfileFeature.js
@@ -1,51 +1,61 @@
-'use strict'
-const User = use('App/Models/User')
-
+"use strict";
+const User = use("App/Models/User");
 
 class FetchProfileFeature {
-    constructor (  request, response, auth ) {
-        this.request = request
-        this.response = response
-        this.auth = auth
+  constructor(request, response, auth) {
+    this.request = request;
+    this.response = response;
+    this.auth = auth;
+  }
+
+  async fetchProfile() {
+    try {
+      const user_id = this.auth.current.user.id;
+      const profile = await User.query()
+        .select(
+          "users.id",
+          "email",
+          "phone_number",
+          "users.is_activated_at",
+          "country_codes.dial_code",
+          "users.phone_number ",
+          "role_label",
+          "first_name",
+          "last_name",
+          "states.state_label",
+          "provinces.province_label",
+          "country_codes.name as country",
+          "stores.id as store_id",
+          "stores.is_activated_at as store_activated_at",
+          "gender",
+          "balance as wallet_balance"
+        )
+        .where("users.id", user_id)
+        .innerJoin("roles", "users.role_id", "roles.id")
+        .innerJoin("wallets", "users.id", "wallets.user_id")
+        .innerJoin("profiles", "users.id", "profiles.user_id")
+        .innerJoin("country_codes", "profiles.country_id", "country_codes.id")
+        .innerJoin("states", "profiles.state_id", "states.id")
+        .innerJoin("provinces", "profiles.province_id", "provinces.id")
+        .leftJoin("stores", "users.id", "stores.user_id")
+        .fetch();
+
+      const serializedResult = profile.toJSON();
+
+      return this.response.status(200).send({
+        message: "Successfully fetched the users profile",
+        status_code: 200,
+        status: "success",
+        result: serializedResult,
+      });
+    } catch (error) {
+      console.log("profile Error -> ", error);
+      return this.response.status(500).send({
+        status: "fail",
+        status_code: 500,
+        message: "Internal Server Error",
+      });
     }
-
-    async fetchProfile () {
-      try {
-        const user_id = this.auth.current.user.id
-        const profile = await User
-        .query()
-        .select('users.id','email','phone_number',
-        'is_activated_at','country_codes.dial_code','users.phone_number ' , 'role_label','first_name',
-        'last_name','states.state_label','provinces.province_label','country_codes.name as country',
-        'gender','balance as wallet_balance')
-        .where('users.id',user_id)
-        .innerJoin('roles', 'users.role_id','roles.id')
-        .innerJoin('wallets', 'users.id','wallets.user_id')
-        .innerJoin('profiles', 'users.id','profiles.user_id')
-        .innerJoin('country_codes', 'profiles.country_id','country_codes.id')
-        .innerJoin('states', 'profiles.state_id','states.id')
-        .innerJoin('provinces', 'profiles.province_id','provinces.id')
-        .fetch()
-
-        const serializedResult = profile.toJSON()
-
-
-        return this.response.status(200).send({
-          message: 'Successfully fetched the users profile',
-          status_code: 200,
-          status: 'success',
-          result:  serializedResult
-        })
-      } catch (error) {
-        console.log('profile Error -> ', error);
-        return this.response.status(500).send({
-          status: 'fail',
-          status_code: 500,
-          message: 'Internal Server Error'
-        })
-      }
-    }
-
-  
+  }
 }
-module.exports = FetchProfileFeature
+module.exports = FetchProfileFeature;
--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -14,52 +14,88 @@
 const Factory = use("Factory");
 const Database = use("Database");
 const Hash = use("Hash");
-const moment = require('moment')
+const moment = require("moment");
 
 class UserSeeder {
   async run() {
     await Database.raw("SET FOREIGN_KEY_CHECKS = 0;");
-	await Database.truncate("users");
-	await Database.truncate("profiles");
-	await Database.truncate("wallets");
+    await Database.truncate("users");
+    await Database.truncate("profiles");
+    await Database.truncate("wallets");
 
-const superAdminPassword = await Hash.make("53cr3t");
-		const country_id = 1
+    const password = await Hash.make("secret");
 
-		const user = await Database
-		.table('users')
-		.insert([
-			{
-				password: superAdminPassword,
-				phone_number: 8101075052,
-				email: "olamartins@gmail.com",
-				is_activated_at: 1,
-				role_id: 1,
-				is_activated_at:   moment().format('YYYY-MM-DD HH:mm:ss'),
-			},
-		])
-		const profiles = await Database
-		.table('profiles')
-		.insert([
-			{
-				user_id: 1,
-				first_name: "Ola",
-				last_name: "Martins",
-				country_id,
-				province_id: 1,
-				state_id: 1,
-				gender: "Male"
-			},
-		])
+    await Database.table("users").insert([
+      {
+        password,
+        phone_number: 8101075052,
+        email: "olamartins@gmail.com",
+        is_activated_at: 1,
+        role_id: 1,
+        is_activated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+      },
+      {
+        password,
+        phone_number: 8075662786,
+        email: "ogbiyoyosky@gmail.com",
+        is_activated_at: 1,
+        role_id: 2,
+        is_activated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+      },
+      {
+        password,
+        phone_number: 8165634196,
+        email: "haloendeliveries@gmail.com",
+        is_activated_at: 1,
+        role_id: 3,
+        is_activated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+      },
+    ]);
 
-		const wallet = await Database
-		.table('wallets')
-		.insert([
-			{
-				user_id: 1,
-				balance: 0
-			},
-		])
+    await Database.table("profiles").insert([
+      {
+        user_id: 1,
+        first_name: "Ola",
+        last_name: "Martins",
+        country_id: 1,
+        province_id: 1,
+        state_id: 1,
+        gender: "Male",
+      },
+      {
+        user_id: 2,
+        first_name: "Emmanuel",
+        last_name: "Ogbiyoyo",
+        country_id: 1,
+        province_id: 1,
+        state_id: 1,
+        gender: "Male",
+      },
+      {
+        user_id: 3,
+        first_name: "Caleb",
+        last_name: "Mathew",
+        country_id: 1,
+        province_id: 1,
+        state_id: 1,
+        gender: "Male",
+      },
+    ]);
+
+    await Database.table("wallets").insert([
+      {
+        user_id: 1,
+        balance: 0,
+      },
+      {
+        user_id: 2,
+        balance: 0,
+      },
+      {
+        user_id: 3,
+        balance: 0,
+      },
+    ]);
 
     await Database.raw("SET FOREIGN_KEY_CHECKS = 1;");
   }
