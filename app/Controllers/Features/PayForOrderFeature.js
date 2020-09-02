bd71279575fd0b837403559fa47398a58a6c0153
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-09-02 03:52:47 +0100

    feature(wallet-cashflows):manage wallet cashflows
    - define a one-to-many relationship between the `wallets` table
    and the `wallet_cashflows` table
    
    - implement a `getTotalUnclearedInflow` getter in the `Wallet` model to
    calculate the sum of all credit cashflows that have not been cleared by
    the system
    
    - implement a `getLedgerBalance` getter in the `Wallet` model to
    calculate the wallet ledger balance based on all inflows and outflows.
    The return value of the getter is equal to the wallet's balance plus
    any uncleared inflows minus all outflows
    
    - implement a `getCashflows` getter in the `Wallet` model that returns
    an array of all cashflows

--- app/Controllers/Features/PayForOrderFeature.js
+++ app/Controllers/Features/PayForOrderFeature.js
@@ -1,12 +1,12 @@
-"use strict";
-const Order = use("App/Models/Order");
-const Store = use("App/Models/Store");
-const OrderNotification = use("App/Models/OrderNotification");
-const moment = require("moment");
-const Wallet = use("App/Models/Wallet");
-const User = use("App/Models/User");
-const Role = use("App/Models/Role");
-const Event = use("Event");
+'use strict';
+const Order = use('App/Models/Order');
+const Store = use('App/Models/Store');
+const OrderNotification = use('App/Models/OrderNotification');
+const moment = require('moment');
+const Wallet = use('App/Models/Wallet');
+const User = use('App/Models/User');
+const Role = use('App/Models/Role');
+const Event = use('Event');
 
 class PayForOrderFeature {
   constructor(request, response, auth) {
@@ -20,22 +20,30 @@ class PayForOrderFeature {
       const { order_id } = this.request.all();
       const { id } = this.auth.current.user;
 
-      const order = await Order.findBy("id", order_id);
+      const order = await Order.findBy('id', order_id);
+
+      if (!order) {
+        return this.response.status(400).send({
+          message: 'Order does not exist',
+          status_code: 400,
+          status: 'fail',
+        });
+      }
 
       if (!order.shipping_cost === null) {
         return this.response.status(400).send({
-          message: "No shipping fee added for this order.",
+          message: 'No shipping fee added for this order.',
           status_code: 400,
-          status: "fail"
+          status: 'fail',
         });
       }
 
       if (order) {
         if (order.is_paid_at) {
           return this.response.status(400).send({
-            message: "Order already paid by you.",
+            message: 'Order already paid by you.',
             status_code: 400,
-            status: "fail"
+            status: 'fail',
           });
         }
         const { shipping_cost, vat, service_charge } = order;
@@ -43,47 +51,47 @@ class PayForOrderFeature {
         const amountToBeBilled =
           order.amount + shipping_cost + vat + service_charge;
 
-        const userWallet = await Wallet.findBy("user_id", id);
+        const userWallet = await Wallet.findBy('user_id', id);
         if (userWallet.balance < amountToBeBilled) {
           return this.response.status(400).send({
-            message: "Insufficient balance",
+            message: 'Insufficient balance',
             status_code: 400,
-            status: "fail"
+            status: 'fail',
           });
         }
 
         userWallet.balance = userWallet.balance - amountToBeBilled;
         await userWallet.save();
 
-        order.is_paid_at = moment().format("YYYY-MM-DD HH:mm:ss");
+        order.is_paid_at = moment().format('YYYY-MM-DD HH:mm:ss');
         await order.save();
 
         //shop details
 
         const notification = await OrderNotification.findBy(
-          "order_id",
+          'order_id',
           order_id
         );
 
         //credit seller
         const sellerId = notification.seller_id;
-        const user = await User.findBy("id", sellerId);
-        const sellerWallet = await Wallet.findBy("user_id", user.id);
+        const user = await User.findBy('id', sellerId);
+        const sellerWallet = await Wallet.findBy('user_id', user.id);
 
         sellerWallet.balance += amountToBeBilled;
         await sellerWallet.save();
 
-        const store = await Store.findBy("user_id", sellerId);
+        const store = await Store.findBy('user_id', sellerId);
 
         const orderDetails = await Order.query()
-          .where("id", order_id)
-          .with("order_notification", builder => {
-            builder.with("buyer_details.profile");
-            builder.with("seller_details.profile");
-            builder.with("order_address.country_code");
-            builder.with("order_address.state");
-            builder.with("order_address.province");
-            builder.with("order_items.main_product_images");
+          .where('id', order_id)
+          .with('order_notification', (builder) => {
+            builder.with('buyer_details.profile');
+            builder.with('seller_details.profile');
+            builder.with('order_address.country_code');
+            builder.with('order_address.state');
+            builder.with('order_address.province');
+            builder.with('order_items.main_product_images');
           })
           .first();
 
@@ -93,7 +101,7 @@ class PayForOrderFeature {
 
         const mailDetails = {
           shop: {
-            shop_name: store.store_name
+            shop_name: store.store_name,
           },
 
           user: {
@@ -103,11 +111,12 @@ class PayForOrderFeature {
             last_name:
               serializedOrderDetails.order_notification.buyer_details.profile
                 .last_name,
-            email: serializedOrderDetails.order_notification.buyer_details.email
+            email:
+              serializedOrderDetails.order_notification.buyer_details.email,
           },
 
           date: moment(serializedOrderDetails.is_paid_at).format(
-            "MMMM Do YYYY"
+            'MMMM Do YYYY'
           ),
 
           order_details: serializedOrderDetails,
@@ -120,31 +129,25 @@ class PayForOrderFeature {
 
           service_charge: service_charge,
 
-          total: amountToBeBilled
+          total: amountToBeBilled,
         };
 
-        Event.fire("new::order", mailDetails);
+        Event.fire('new::order', mailDetails);
 
         return this.response.status(200).send({
           message: `Successfully paid for order  ${order.placement_code}`,
           status_code: 200,
-          status: "success"
+          status: 'success',
         });
       }
 
       //send email
-
-      return this.response.status(400).send({
-        message: "Order does not exist",
-        status_code: 400,
-        status: "fail"
-      });
     } catch (payForOrderError) {
-      console.log("payForOrderError", payForOrderError);
+      console.log('payForOrderError', payForOrderError);
       return this.response.status(500).send({
-        status: "Fail",
-        message: "Internal Server Error",
-        status_code: 500
+        status: 'Fail',
+        message: 'Internal Server Error',
+        status_code: 500,
       });
     }
   }
---
+++ tryouts.js
@@ -0,0 +1,69 @@
+const getLedgerBalance = ({ balance, cashflows }) => {
+  const sumOfUnclearedWalletInflows = cashflows.reduce(
+    (accumulator, cashflow) => {
+      if (cashflow.type === 'credit') {
+        return accumulator + cashflow.amount;
+      }
+
+      if (cashflow.type === 'debit') {
+        return accumulator - cashflow.amount;
+      }
+    },
+    0
+  );
+
+  return sumOfUnclearedWalletInflows;
+};
+
+const getTotalUnclearedInflows = ({ balance, cashflows }) => {
+  const sumOfUnclearedWalletInflows = cashflows.reduce(
+    (accumulator, cashflow) => {
+      if (cashflow.type === 'credit' && cashflow.is_cleared === false) {
+        return accumulator + cashflow.amount;
+      }
+
+      return accumulator;
+    },
+    0
+  );
+
+  return sumOfUnclearedWalletInflows;
+};
+
+const wallet = {
+  balance: 10,
+  cashflows: [
+    {
+      amount: 30,
+      type: 'credit',
+      is_cleared: true,
+    },
+    {
+      amount: 10,
+      type: 'debit',
+      is_cleared: true,
+    },
+    {
+      amount: 20,
+      type: 'credit',
+      is_cleared: true,
+    },
+    {
+      amount: 15,
+      type: 'credit',
+      is_cleared: true,
+    },
+    {
+      amount: 5,
+      type: 'debit',
+      is_cleared: true,
+    },
+  ],
+};
+
+const ledgerBalance = getLedgerBalance(wallet);
+const totalUnclearedInflows = getTotalUnclearedInflows(wallet);
+
+console.log('Ledger balance:', ledgerBalance);
+console.log('Uncleared balance:', totalUnclearedInflows);
+console.log('Available balance:', ledgerBalance - totalUnclearedInflows);
