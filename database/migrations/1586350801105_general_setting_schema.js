18b564384e4f259afe05c8cfe03483dda1471023
Caleb Mathew <creatrixity@gmail.com>
2022-02-04 20:32:13 +0100

    chore: update service charge

--- database/migrations/1586350801105_general_setting_schema.js
+++ database/migrations/1586350801105_general_setting_schema.js
@@ -1,21 +1,21 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class GeneralSettingSchema extends Schema {
   up() {
-    this.create('general_settings', (table) => {
-      table.increments()
-      table.integer("vat").defaultTo(5)
-      table.integer("service_charge").defaultTo(2)
-      table.timestamps()
-    })
+    this.create("general_settings", (table) => {
+      table.increments();
+      table.integer("vat").defaultTo(5);
+      table.integer("service_charge").defaultTo(500);
+      table.timestamps();
+    });
   }
 
   down() {
-    this.drop('general_settings')
+    this.drop("general_settings");
   }
 }
 
-module.exports = GeneralSettingSchema
+module.exports = GeneralSettingSchema;
